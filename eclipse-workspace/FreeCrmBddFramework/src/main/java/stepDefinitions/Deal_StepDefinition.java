698b772388af5fef3c63b1a59c2c480078218db2
Praveen Das <eckpraveen@gmail.com>
2020-01-22 19:58:20 +0530

    "Commiting Final changes in FreeCRM Project"

--- eclipse-workspace/FreeCrmBddFramework/src/main/java/stepDefinitions/Deal_StepDefinition.java
+++ eclipse-workspace/FreeCrmBddFramework/src/main/java/stepDefinitions/Deal_StepDefinition.java
@@ -1,104 +1,104 @@
-package stepDefinitions;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import cucumber.api.DataTable;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import junit.framework.Assert;
-
-public class Deal_StepDefinition {
-	WebDriver driver;
-	JavascriptExecutor js;
-
-	
-	
-	@Given("^User is already present on login page$")
-	public void User_alread_on_Login_page() {		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\praveen.das\\eclipse-workspace\\FreeCrmBddFramework\\src\\chromedriver.exe");
-		driver= new ChromeDriver();
-		driver.get("https://ui.freecrm.com/");
-		driver.manage().window().maximize();
-	}
-	
-	@SuppressWarnings("deprecation")
-	@When("^title of login page is Free CRM$")
-	public void Title_check_on_LoginPage() { 
-		String title= driver.getTitle();
-		System.out.println(title);
-		Assert.assertEquals("Cogmento CRM", title);
-	}
-
-	
-	@Then("^user enters username and password$")
-	public void Enter_UN_and_PWD(DataTable credentials) {
-		List<List<String>> data = credentials.raw(); //from java.util
-		driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(data.get(0).get(0));
-		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys(data.get(0).get(1));	
-	}
-	
-	@Then("^user clicks on Login button$")
-	public void Click_on_Login() throws InterruptedException {
-		Thread.sleep(5000);
-		WebElement loginButton= driver.findElement(By.xpath("//div[@class=\"ui fluid large blue submit button\"]"));	
-		//loginButton.click();
-	    JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click();", loginButton);		
-	}
-
-	@And("^user is on Homepage$")
-	public void User_is_on_homePage() {
-		String title=driver.getTitle();
-		Assert.assertEquals("Cogmento CRM", title);
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Then("^user moves to new deal page$")
-	public void Move_on_New_DealPage() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);				
-		driver.findElement(By.xpath("//*[@class='ui fluid container']/div/a[5]")).click();
-		Thread.sleep(10000);
-		driver.findElement(By.xpath("//*[text()='New']")).click();
-		//Thread.sleep(5000);
-		String CreateNewDealText= driver.findElement(By.xpath("//*[text()='Create new Deal']")).getText();
-		Assert.assertEquals("Create new Deal", CreateNewDealText);
-		//Thread.sleep(7000);
-	}
-	
-	@Then("^user enters deal details- Title and Company and Probability and Amount and Commission and Stage$")
-	public void User_enters_deal_deatils(DataTable dealData) throws InterruptedException {
-		List<List<String>> dealValues = dealData.raw();
-		
-		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);				
-		WebDriverWait wait = new WebDriverWait(driver, 15000);
-
-		driver.findElement(By.name("title")).sendKeys(dealValues.get(0).get(0));
-		
-		//wait.until(ExpectedConditions.elementToBeClickable(By.name("company")));
-		//driver.findElement(By.name("company")).sendKeys(dealValues.get(0).get(1));
-		//*[@name='company']/div[2] probability amount commission 
-		driver.findElement(By.xpath("//div[@name='company']//input[@class='search']")).sendKeys(dealValues.get(0).get(1));
-		driver.findElement(By.name("probability")).sendKeys(dealValues.get(0).get(2));
-		driver.findElement(By.name("amount")).sendKeys(dealValues.get(0).get(3));
-		driver.findElement(By.name("commission")).sendKeys(dealValues.get(0).get(4));
-		driver.findElement(By.name("stage")).sendKeys(dealValues.get(0).get(5));
-		driver.findElement(By.xpath("//button[text()='Save']")).click();
-		Thread.sleep(5000);		
-	}
-	
-	@Then("^user close the browser$")
-	public void TearDown() {
-		driver.close();
-	}
-}
+//package stepDefinitions;
+//
+//import java.util.List;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import cucumber.api.DataTable;
+//import cucumber.api.java.en.And;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+//import junit.framework.Assert;
+//
+//public class Deal_StepDefinition {
+//	WebDriver driver;
+//	JavascriptExecutor js;
+//
+//	
+//	
+//	@Given("^User is already present on login page$")
+//	public void User_alread_on_Login_page() {		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\praveen.das\\eclipse-workspace\\FreeCrmBddFramework\\src\\chromedriver.exe");
+//		driver= new ChromeDriver();
+//		driver.get("https://ui.freecrm.com/");
+//		driver.manage().window().maximize();
+//	}
+//	
+//	@SuppressWarnings("deprecation")
+//	@When("^title of login page is Free CRM$")
+//	public void Title_check_on_LoginPage() { 
+//		String title= driver.getTitle();
+//		System.out.println(title);
+//		Assert.assertEquals("Cogmento CRM", title);
+//	}
+//
+//	
+//	@Then("^user enters username and password$")
+//	public void Enter_UN_and_PWD(DataTable credentials) {
+//		List<List<String>> data = credentials.raw(); //from java.util
+//		driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(data.get(0).get(0));
+//		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys(data.get(0).get(1));	
+//	}
+//	
+//	@Then("^user clicks on Login button$")
+//	public void Click_on_Login() throws InterruptedException {
+//		Thread.sleep(5000);
+//		WebElement loginButton= driver.findElement(By.xpath("//div[@class=\"ui fluid large blue submit button\"]"));	
+//		//loginButton.click();
+//	    JavascriptExecutor js=(JavascriptExecutor) driver;
+//		js.executeScript("arguments[0].click();", loginButton);		
+//	}
+//
+//	@And("^user is on Homepage$")
+//	public void User_is_on_homePage() {
+//		String title=driver.getTitle();
+//		Assert.assertEquals("Cogmento CRM", title);
+//	}
+//	
+//	@SuppressWarnings("deprecation")
+//	@Then("^user moves to new deal page$")
+//	public void Move_on_New_DealPage() throws InterruptedException {
+//		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);				
+//		driver.findElement(By.xpath("//*[@class='ui fluid container']/div/a[5]")).click();
+//		Thread.sleep(10000);
+//		driver.findElement(By.xpath("//*[text()='New']")).click();
+//		//Thread.sleep(5000);
+//		String CreateNewDealText= driver.findElement(By.xpath("//*[text()='Create new Deal']")).getText();
+//		Assert.assertEquals("Create new Deal", CreateNewDealText);
+//		//Thread.sleep(7000);
+//	}
+//	
+//	@Then("^user enters deal details- Title and Company and Probability and Amount and Commission and Stage$")
+//	public void User_enters_deal_deatils(DataTable dealData) throws InterruptedException {
+//		List<List<String>> dealValues = dealData.raw();
+//		
+//		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);				
+//		WebDriverWait wait = new WebDriverWait(driver, 15000);
+//
+//		driver.findElement(By.name("title")).sendKeys(dealValues.get(0).get(0));
+//		
+//		//wait.until(ExpectedConditions.elementToBeClickable(By.name("company")));
+//		//driver.findElement(By.name("company")).sendKeys(dealValues.get(0).get(1));
+//		//*[@name='company']/div[2] probability amount commission 
+//		driver.findElement(By.xpath("//div[@name='company']//input[@class='search']")).sendKeys(dealValues.get(0).get(1));
+//		driver.findElement(By.name("probability")).sendKeys(dealValues.get(0).get(2));
+//		driver.findElement(By.name("amount")).sendKeys(dealValues.get(0).get(3));
+//		driver.findElement(By.name("commission")).sendKeys(dealValues.get(0).get(4));
+//		driver.findElement(By.name("stage")).sendKeys(dealValues.get(0).get(5));
+//		driver.findElement(By.xpath("//button[text()='Save']")).click();
+//		Thread.sleep(5000);		
+//	}
+//	
+//	@Then("^user close the browser$")
+//	public void TearDown() {
+//		driver.close();
+//	}
+//}
--- eclipse-workspace/FreeCrmBddFramework/test-output/report.js
+++ eclipse-workspace/FreeCrmBddFramework/test-output/report.js
@@ -1,135 +1,255 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/praveen.das/eclipse-workspace/FreeCrmBddFramework/src/main/java/Features/Deals.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/praveen.das/eclipse-workspace/FreeCrmBddFramework/src/main/java/Features/TaggedHooks.feature");
 formatter.feature({
   "line": 1,
-  "name": "Deal_data creation",
+  "name": "Test Tagged Hooks",
   "description": "",
-  "id": "deal-data-creation",
+  "id": "test-tagged-hooks",
   "keyword": "Feature"
 });
+formatter.before({
+  "duration": 173600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 74300,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 52700,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 40500,
+  "status": "passed"
+});
 formatter.scenario({
-  "line": 3,
-  "name": "FreeCRM create a new Deal scenario",
+  "line": 4,
+  "name": "This is first test.",
   "description": "",
-  "id": "deal-data-creation;freecrm-create-a-new-deal-scenario",
+  "id": "test-tagged-hooks;this-is-first-test.",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@First"
+    }
+  ]
 });
 formatter.step({
   "line": 5,
-  "name": "User is already present on login page",
+  "name": "this is the first step",
   "keyword": "Given "
 });
 formatter.step({
   "line": 6,
-  "name": "title of login page is Free CRM",
+  "name": "this is the second step",
   "keyword": "When "
 });
 formatter.step({
   "line": 7,
-  "name": "user enters username and password",
-  "rows": [
-    {
-      "cells": [
-        "eckpraveen@gmail.com",
-        "Pdv@1992"
-      ],
-      "line": 8
-    }
-  ],
+  "name": "this is the third step",
   "keyword": "Then "
 });
-formatter.step({
-  "line": 9,
-  "name": "user clicks on Login button",
-  "keyword": "Then "
+formatter.match({
+  "location": "TaggedHooks_StepDefinition.this_is_the_first_step()"
 });
-formatter.step({
+formatter.result({
+  "duration": 78948300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooks_StepDefinition.this_is_the_second_step()"
+});
+formatter.result({
+  "duration": 67800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TaggedHooks_StepDefinition.this_is_the_third_step()"
+});
+formatter.result({
+  "duration": 65600,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 109400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 84100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 62900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 61600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 77200,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 55700,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 47100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 70500,
+  "status": "passed"
+});
+formatter.scenario({
   "line": 10,
-  "name": "user is on Homepage",
-  "keyword": "And "
+  "name": "This is second test.",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-second-test.",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@Second"
+    }
+  ]
 });
 formatter.step({
   "line": 11,
-  "name": "user moves to new deal page",
-  "keyword": "Then "
+  "name": "this is the first step",
+  "keyword": "Given "
 });
 formatter.step({
   "line": 12,
-  "name": "user enters deal details- Title and Company and Probability and Amount and Commission and Stage",
-  "rows": [
-    {
-      "cells": [
-        "PNB",
-        "Incop",
-        "5",
-        "60000",
-        "10.1",
-        "Prospect"
-      ],
-      "line": 13
-    }
-  ],
-  "keyword": "Then "
+  "name": "this is the second step",
+  "keyword": "When "
 });
 formatter.step({
-  "line": 14,
-  "name": "user close the browser",
+  "line": 13,
+  "name": "this is the third step",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "Deal_StepDefinition.User_alread_on_Login_page()"
+  "location": "TaggedHooks_StepDefinition.this_is_the_first_step()"
 });
 formatter.result({
-  "duration": 23029888300,
+  "duration": 75100,
   "status": "passed"
 });
 formatter.match({
-  "location": "Deal_StepDefinition.Title_check_on_LoginPage()"
+  "location": "TaggedHooks_StepDefinition.this_is_the_second_step()"
 });
 formatter.result({
-  "duration": 17225200,
+  "duration": 44600,
   "status": "passed"
 });
 formatter.match({
-  "location": "Deal_StepDefinition.Enter_UN_and_PWD(DataTable)"
+  "location": "TaggedHooks_StepDefinition.this_is_the_third_step()"
 });
 formatter.result({
-  "duration": 361010000,
+  "duration": 40100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 50900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 33700,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 31300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 29400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 67400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 53600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 47900,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 52800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "This is third test.",
+  "description": "",
+  "id": "test-tagged-hooks;this-is-third-test.",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@Third"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "this is the first step",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "this is the second step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "this is the third step",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaggedHooks_StepDefinition.this_is_the_first_step()"
+});
+formatter.result({
+  "duration": 193600,
   "status": "passed"
 });
 formatter.match({
-  "location": "Deal_StepDefinition.Click_on_Login()"
+  "location": "TaggedHooks_StepDefinition.this_is_the_second_step()"
 });
 formatter.result({
-  "duration": 5085626900,
+  "duration": 58700,
   "status": "passed"
 });
 formatter.match({
-  "location": "Deal_StepDefinition.User_is_on_homePage()"
+  "location": "TaggedHooks_StepDefinition.this_is_the_third_step()"
 });
 formatter.result({
-  "duration": 11166400,
+  "duration": 49400,
   "status": "passed"
 });
-formatter.match({
-  "location": "Deal_StepDefinition.Move_on_New_DealPage()"
-});
-formatter.result({
-  "duration": 13272614800,
+formatter.after({
+  "duration": 81700,
   "status": "passed"
 });
-formatter.match({
-  "location": "Deal_StepDefinition.User_enters_deal_deatils(DataTable)"
-});
-formatter.result({
-  "duration": 5865532200,
+formatter.after({
+  "duration": 57100,
   "status": "passed"
 });
-formatter.match({
-  "location": "Deal_StepDefinition.TearDown()"
+formatter.after({
+  "duration": 52900,
+  "status": "passed"
 });
-formatter.result({
-  "duration": 166548500,
+formatter.after({
+  "duration": 42900,
   "status": "passed"
 });
 });
